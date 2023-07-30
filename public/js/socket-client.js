ccfb005ccd5be04e4581b0d610d41063c05f6c5b
Johann Castillo <jcastvas@emeal.nttdata.com>
2023-07-30 16:48:11 -0500

    first commit

---
+++ public/js/socket-client.js
@@ -0,0 +1,33 @@
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
+const socket = io();
+
+socket.on('connect', () => {
+  lblOffline.style.display = 'none';
+  lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+  console.log('Desconectado');
+  lblOnline.style.display = 'none';
+  lblOffline.style.display = '';
+});
+
+socket.on('enviarmensaje', (payload) => {
+    console.log(payload.mensaje);
+});
+
+btnEnviar.addEventListener('click', () => {
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+    socket.emit('enviar-mensaje', payload, (id) => {
+        console.log('Desde el server', id);
+    });
+});
